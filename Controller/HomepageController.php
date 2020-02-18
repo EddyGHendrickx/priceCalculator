2ebefa3cfa81aec82a7030f1ee3f053699db3e6e
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-02-18 09:50:45 +0100

    is this merge?

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -32,16 +32,14 @@ class HomepageController
             $Product[$i] = new Products($ProductData[$i]['name'], strval($ProductData[$i]['id']), strval($ProductData[$i]['description']), strval($ProductData[$i]['price']));
         }
 
-        for ($i = 0; count($groupData) > $i; $i++) {
-            $Product[$i] = new Group($groupData[$i]['id'], strval($groupData[$i]['name']), strval($groupData[$i]['discount']), strval($groupData[$i]['group_id']));
-        }
+//        for ($i = 0; count($groupData) > $i; $i++) {
+//            $Group[$i] = new Group(strval($groupData[$i]['id']), strval($groupData[$i]['name']), strval($groupData[$i]['discount']), strval($groupData[$i]['group_id']));
+//        }
 
 
 
         $groupID = $_POST['customers'] ;
         $originalPrice = $_POST['product'];
-
-
 
 
 
